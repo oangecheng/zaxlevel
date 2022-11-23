81f97d7f2ab6b36c355c714b27ed11173492f220
chengzhi <chengzhi@kuaishou.com>
2022-11-23 17:48:53 +0800

    武器升级攻击力策略调整

--- scripts/components/zxdamage.lua
+++ scripts/components/zxdamage.lua
@@ -1,13 +1,21 @@
 -- 武器提升攻击力组件
 -- 给予其他物品提升攻击力，上限80点
 
--- 最大攻击力80
-local MAX_DAMAGE = 80
+-- 最大攻击力100
+local MAX_DAMAGE = 100
+
+-- 武器升级的阈值和倍率
+-- 51之前一个铥矿棒能够加 4 攻击力， 68之前为2， 80之前为1
+local DAMAGE_NORMAL = {51, 1}
+local DAMAGE_SENIOR = {68, 0.5}
+local DAMAGE_HIGH = {80, 0.25}
+local DAMAGE_SUPER = {MAX_DAMAGE, 0.1}
+
 
 -- 可以提升攻击力的物品
 local ITEM_DEFS = {
-	{"ruins_bat", 0.5}, --铥矿棒
-	{"tentaclespike", 0.02}, --触手尖刺
+	{"ruins_bat", 4}, --铥矿棒
+	{"tentaclespike", 2}, --触手尖刺
 }
 
 
@@ -53,8 +61,28 @@ end
 local function itemGive(inst, giver, item)
     local target = findTargetItem(item)
     if target ~= nil then
+
         local zxdamage = inst.components.zxdamage
-        zxdamage.damage = math.min(zxdamage.damage + target[2], MAX_DAMAGE)
+        local damage = target[2]
+        local policy = nil
+
+        
+        if zxdamage < DAMAGE_NORMAL[1] then
+            policy = DAMAGE_NORMAL
+        elseif zxdamage < DAMAGE_SENIOR[1] then
+            policy = DAMAGE_SENIOR
+        elseif zxdamage < DAMAGE_HIGH[1] then
+            policy = DAMAGE_HIGH
+        else 
+            policy = DAMAGE_SUPER
+        end
+
+
+        if policy ~= nil then
+            damage = damage * policy[2]
+            zxdamage.damage = math.min(zxdamage.damage + damage, policy[1])
+        end
+
         giver.components.talker:Say("这武器锻造的越来越完美了！")
         updateDamage(zxdamage)
     end
